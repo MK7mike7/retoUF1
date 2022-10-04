a061e88457f32b35e92c64eebeb9de4d44b3905e
raul lopez <raul.lopez.isidro@gmail.com>
2022-10-04 17:51:02 +0200

    Ejercicio reto UF1

---
+++ src/Start.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Start {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int hora = 0;
+		int minutos = 0;
+		int segundos = 0;
+ 
+		Scanner entradaDatos = new Scanner(System.in);
+ 
+		System.out.print("Introduzca la hora:");
+		hora = entradaDatos.nextInt();
+ 
+		System.out.print("Introduzca los minutos:");
+		minutos = entradaDatos.nextInt();
+ 
+		System.out.print("Introduzca los segundos:");
+		segundos = entradaDatos.nextInt();
+ 
+		if ((hora >= 0 && hora <= 23) && (minutos >= 0 && minutos <= 59) && (segundos >= 0 && segundos <= 59)) {
+ 
+			System.out.println ("La hora es correcta:" + hora + ":" + minutos + ":" + segundos );
+ 
+		} else 
+			
+			System.out.println("La hora NO es correcta");
+ 
+	}
+
+}
